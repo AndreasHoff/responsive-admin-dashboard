8bef599f7158840ffd1981e0889bc9a4c424e7ae
AndreasHoff <mchoffn@hotmail.com>
2023-11-19 16:16:24 +0100

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Javascript Tutorial',
+        productNumber: '424242',
+        paymentStatus: 'Paid',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '94624',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flexbox Tutorial',
+        productNumber: '157342',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
